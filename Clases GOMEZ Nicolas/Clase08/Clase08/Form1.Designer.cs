db989a23e0b6726528071f72eab1b543a95ef382
Nicolas Gomez <niconetgomez@hotmail.com>
2018-04-18 22:11:26 -0300

    ejercicio 28 Dictionary
    Como usar un dictionary... inicializar, cargar y aumentar valores de repeticion.

---
+++ Clases GOMEZ Nicolas/Clase08/Clase08/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Clase08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 312);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(355, 317);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 352);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Clases GOMEZ Nicolas/Clase08/Clase08/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int i, flag = 0;
+            Dictionary<string, int> diccionario = new Dictionary<string,int>();
+            int inicio = 0;
+            int final;
+            string palabra = "";
+            
+
+            for (i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (textBox1.Text[i] == ' ' || i == textBox1.Text.Length - 1)
+                {
+                    final = i;
+
+                    palabra = textBox1.Text.Substring(inicio, final + 1 - inicio);
+                    palabra = palabra.Trim(); //saca los espacios en blanco.
+                    
+                    foreach (KeyValuePair<string, int> entrada in diccionario)
+                    {
+                        flag = 0;
+                        if((palabra == entrada.Key))
+                        {
+                            //diccionario.ContainsKey() ahorra el foreach
+                            diccionario[entrada.Key]++;                            
+                            flag = 1;
+                            break;
+                        }                        
+                    }
+                    if (flag == 0)
+                    {
+                        diccionario.Add(palabra, 1);
+                    }
+
+                    inicio = i + 1;
+                }                
+            }
+
+             foreach (KeyValuePair<string, int> entrada in diccionario)
+            {
+                MessageBox.Show(entrada.Key, entrada.Value.ToString());                
+            }
+        }
+    }
+}
