123c785a51e919217037428ca9c6aa231a854d72
Nicolas Gomez <niconetgomez@hotmail.com>
2018-06-06 22:10:12 -0300

    Agrego la parte de Archivo de texto

---
+++ Clases GOMEZ Nicolas/Clase17/IO/ArchivoTexto.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+
+namespace IO
+{
+    public static class ArchivoTexto
+    {
+        public static bool Guardar(string ruta, string texto)
+        {
+            try
+            {
+                XmlTextWriter writer = new XmlTextWriter(ruta, Encoding.UTF8);
+                XmlSerializer ser = new XmlSerializer(typeof(string));
+
+                ser.Serialize(writer, texto);
+
+                writer.Close();
+            }
+            catch (ArgumentException)
+            {
+                string fallo = "Error en la ruta";
+                throw new ArgumentException(fallo);
+            }
+            catch (FileNotFoundException)
+            {
+                string fallo = "Error en la ruta";
+                throw new FileNotFoundException(fallo);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                string fallo = "Error en la ruta";
+                throw new DirectoryNotFoundException(fallo);
+            }
+            catch (IOException)
+            {
+                string fallo = "Archivo en uso";
+                throw new IOException(fallo);
+            }
+            catch (NotSupportedException)
+            {
+                string fallo = "La ruta contiene caracteres no aceptados";
+                throw new NotSupportedException(fallo);
+            }
+            catch (System.Security.SecurityException)
+            {
+                string fallo = "El usuario no tiene los permisos necesarios";
+                throw new System.Security.SecurityException(fallo);
+            }
+            catch (InvalidOperationException)
+            {
+                string fallo = "Nombre no encontrado";
+                throw new InvalidOperationException(fallo);
+            }
+
+            return true;
+        }
+
+
+
+        public static string Leer(string ruta)
+        {
+            string aux;
+
+            try
+            {
+                XmlTextReader reader = new XmlTextReader(ruta);
+                XmlSerializer ser = new XmlSerializer(typeof(string));
+
+                aux = (string) ser.Deserialize(reader);
+
+                reader.Close();
+            }
+            catch (ArgumentException)
+            {
+                string fallo = "Error en la ruta";
+                throw new ArgumentException(fallo);
+            }
+            catch (FileNotFoundException)
+            {
+                string fallo = "Error en la ruta";
+                throw new FileNotFoundException(fallo);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                string fallo = "Error en la ruta";
+                throw new DirectoryNotFoundException(fallo);
+            }
+            catch (IOException)
+            {
+                string fallo = "Archivo en uso";
+                throw new IOException(fallo);
+            }
+            catch (NotSupportedException)
+            {
+                string fallo = "La ruta contiene caracteres no aceptados";
+                throw new NotSupportedException(fallo);
+            }
+            catch (System.Security.SecurityException)
+            {
+                string fallo = "El usuario no tiene los permisos necesarios";
+                throw new System.Security.SecurityException(fallo);
+            }
+            catch (InvalidOperationException)
+            {
+                string fallo = "Nombre no encontrado";
+                throw new InvalidOperationException(fallo);
+            }
+
+            return aux;
+        }
+
+
+    }
+}
---
+++ Clases GOMEZ Nicolas/Clase17/IO/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            string nombreArchivo;
+            string extencion = ".txt";
+            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
+            try
+            {               
+                /*
+                Console.WriteLine("1ro escribir nombre de archivo, luego el texto deseado");
+
+                nombreArchivo = Console.ReadLine();
+
+                texto = Console.ReadLine();
+
+                ruta = ruta + "\\" + nombreArchivo + extencion;
+
+                ArchivoTexto.Guardar(ruta, texto);
+                */
+
+                nombreArchivo = "buen";
+
+                ruta = ruta + "\\" + nombreArchivo + extencion;
+
+                texto = ArchivoTexto.Leer(ruta);
+
+                Console.WriteLine(texto);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                nombreArchivo = DateTime.Now.ToString();
+                ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                ruta = ruta + "\\" + nombreArchivo + extencion;
+                ArchivoTexto.Guardar(ruta, e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
